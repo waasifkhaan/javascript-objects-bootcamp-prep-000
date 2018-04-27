5f7997c8aca56dcbc8f6514efe23b8ee39e9ce71
Wasif Khan <waasif.khaan@gmail.com>
2018-04-27 02:35:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {name:"song"}
 function updatePlaylist( playlist, artistName, songTitle) {
 return Object.assign(playlist, { [artistName]: songTitle})
   }
-function removeFromPlaylist(playlist, artistName) {
-   delete playlist[artistName];
-return playlist
-  }
+// function removeFromPlaylist(playlist, artistName) {
+//   delete playlist[artistName];
+// return playlist
+//   }
